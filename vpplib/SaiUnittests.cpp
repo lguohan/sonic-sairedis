112efdff5a08ad471fb420276a6eb11dc4cfe49e
Guohan Lu <lguohan@gmail.com>
2024-09-17 15:43:02 -0700

    port SAI_OBJECT_TYPE_EXTENSIONS_MAX fix
    Signed-off-by: Guohan Lu <lguohan@gmail.com>

--- vpplib/SaiUnittests.cpp
+++ vpplib/SaiUnittests.cpp
@@ -1,18 +1,3 @@
-/*
- * Copyright 2016 Microsoft, Inc.
- * Modifications copyright (c) 2023 Cisco and/or its affiliates.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at:
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 #include "Sai.h"
 #include "SaiInternal.h"
 
@@ -94,7 +79,7 @@ void Sai::channelOpSetReadOnlyAttribute(
     sai_object_type_t object_type;
     sai_deserialize_object_type(str_object_type, object_type);
 
-    if (object_type == SAI_OBJECT_TYPE_NULL || object_type >= SAI_OBJECT_TYPE_EXTENSIONS_MAX)
+    if (sai_metadata_is_object_type_valid(object_type) == false)
     {
         SWSS_LOG_ERROR("invalid object type: %d", object_type);
         return;
